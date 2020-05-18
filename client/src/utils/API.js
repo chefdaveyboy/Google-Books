84d39bc699fc97a6f5aa9d38f9e2e9e84897f7a1
David Koeller <david.koeller@gmail.com>
2020-05-18 16:08:06 -0500

    added components

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,27 +2,21 @@ import axios from "axios";
 
 // The getBooks method retrieves books from the server
 // It accepts a "query" or term to search the book api for
+import axios from "axios";
+
 export default {
-  getBooks: function(query) {
-    console.log(query)
-    console.log('Connected');
-    return axios.get("/api/booksearch", { params: { q: query } });
-  },
+    getBooks: query => {
+        console.log(query);
+        return axios.get("/api/books");
+    },
+    searchBooks: (title) => {
+        return axios.post("/search", {title: title});
+    },
+    saveBook: (bookData) => {
+        return axios.post("/api/books", bookData);
+    },
+    deleteBook: (id) => {
+        return axios.delete("/api/books/" + id);
+    }
+}
 
-  getSaved: function() {
-    console.log('Got Saved Books');
-    return axios.get("api/saved");
-  },
-
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-      console.log('Book Deleted');
-    return axios.delete("/api/saved/" + id);
-  },
-
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    console.log("Saved a Book");
-    return axios.post("/api/saved", bookData);
-  }
-};
